5d654f7dadebb321ee47c82bb3d0f035b7ff33f8
Ran Guin <rguin@iMac.local>
2021-02-23 00:08:42 -0800

    setup template for npm javascript module

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function addTwoNum(num1, num2) {
+    const total = num1 + num2;
+    return total;
+}
+
+function addThreeNum(num1, num2, num3) {
+    const total = num1 + num2 + num3;
+    return total;
+}
+
+module.exports = {
+    addTwoNum,
+    addThreeNum,
+}
